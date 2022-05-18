945cf71529d40c5c57ae79686da0c7330c531622
Temirlan-qa <79401245+Temirlan-qa@users.noreply.github.com>
2022-05-18 09:36:42 +0600

    Add files via upload

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,7 @@ import 'package:duglinpub_menu/drawer/drawer.dart';
 import 'package:duglinpub_menu/screens/bar/bar.dart';
 import 'package:duglinpub_menu/utils/colors.dart';
 import 'package:duglinpub_menu/screens/kuh/recipe.dart';
+import 'package:duglinpub_menu/views/rules.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -36,206 +37,206 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
       ),
-      body: Container(
-        color: const Color(0xFFE1DAD2),
-        child: Column(
-          children: [
-            InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const BarPage()));
-                // BarPage
-                print('Барное меню');
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-                width: MediaQuery.of(context).size.width,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: mobileBackgroundColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow:[
-                    BoxShadow(
-                      color: mobileBackgroundColor.withOpacity(0.6),
-                      offset: const Offset(
-                        0.0,
-                        10.0,
-                      ),
-                      blurRadius: 10.0,
-                      spreadRadius: -6.0,
-                    ),  
-                  ],
-                  image: DecorationImage(
-                    colorFilter: ColorFilter.mode(
-                      mobileBackgroundColor.withOpacity(0.35),
-                      BlendMode.multiply,
-                    ),
-                    // IMG_7004.PNG
-                    image: const AssetImage('images/IMG_7004.PNG'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Stack(
-                  children: const [
-                    Align(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5.0),
-                        child: Text(
-                          "Барное меню",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w700
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      alignment: Alignment.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: const Color(0xFFE1DAD2),
+          child: Column(
             
-            InkWell(
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const QuizPage()));
-                print('Кухонное меню');
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-                width: MediaQuery.of(context).size.width,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: mobileBackgroundColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow:[
-                    BoxShadow(
-                      color: mobileBackgroundColor.withOpacity(0.6),
-                      offset: const Offset(
-                        0.0,
-                        10.0,
-                      ),
-                      blurRadius: 10.0,
-                      spreadRadius: -6.0,
-                    ),  
-                  ],
-                  image: DecorationImage(
-                    colorFilter: ColorFilter.mode(
-                      mobileBackgroundColor.withOpacity(0.35),
-                      BlendMode.multiply,
-                    ),
-                    image: const NetworkImage('https://immigrantinvest.com/wp-content/uploads/2016/12/50-european-dishes.jpeg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Stack(
-                  children: const [
-                    Align(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5.0),
-                        child: Text(
-                          "Кухонное меню",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w700
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          textAlign: TextAlign.center,
+            children: [
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const BarPage()));
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: mobileBackgroundColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow:[
+                      BoxShadow(
+                        color: mobileBackgroundColor.withOpacity(0.6),
+                        offset: const Offset(
+                          0.0,
+                          10.0,
                         ),
+                        blurRadius: 10.0,
+                        spreadRadius: -6.0,
+                      ),  
+                    ],
+                    image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                        mobileBackgroundColor.withOpacity(0.35),
+                        BlendMode.multiply,
                       ),
-                      alignment: Alignment.center,
+                      // IMG_7004.PNG
+                      image: const AssetImage('images/IMG_7004.PNG'),
+                      fit: BoxFit.cover,
                     ),
-                  ],
-                ),
-              ),
-            ),
-      
-            InkWell(
-              onTap: (){
-                // Navigator.of(context).push(MaterialPageRoute(builder: (context) => const QuizPage()));
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-                width: MediaQuery.of(context).size.width,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: mobileBackgroundColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow:[
-                    BoxShadow(
-                      color: mobileBackgroundColor.withOpacity(0.6),
-                      offset: const Offset(
-                        0.0,
-                        10.0,
-                      ),
-                      blurRadius: 10.0,
-                      spreadRadius: -6.0,
-                    ),  
-                  ],
-                  image: DecorationImage(
-                    colorFilter: ColorFilter.mode(
-                      mobileBackgroundColor.withOpacity(0.35),
-                      BlendMode.multiply,
-                    ),
-                    image: const NetworkImage('https://firebasestorage.googleapis.com/v0/b/dublinpub-a39c8.appspot.com/o/photo_2022-05-17_12-15-42.jpg?alt=media&token=3ac4154e-05b9-41a2-b401-182ff557d92f'),
-                    fit: BoxFit.cover,
                   ),
-                ),
-                child: Stack(
-                  children: const [
-                    Align(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 5.0),
-                        child: Text(
-                          "Все правилы - Dublin Pub",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w700
+                  child: Stack(
+                    children: const [
+                      Align(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Text(
+                            "Барное меню",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
                           ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
-                          textAlign: TextAlign.center,
                         ),
+                        alignment: Alignment.center,
                       ),
-                      alignment: Alignment.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-      
-            const Spacer(),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 22,),
-              width: MediaQuery.of(context).size.width,
-              height: 40,            
-              // color: const Color.fromARGB(255, 40, 99, 42),
-              decoration:  const BoxDecoration(
-                color: Color(0xFF046936), //new Color.fromRGBO(255, 0, 0, 0.0),
-                borderRadius:  BorderRadius.only(
-                  topLeft:  Radius.circular(40.0),
-                  topRight: Radius.circular(40.0))
-              ),
-              child: const Align(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Text(
-                    "Обслуживание 15%",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
+                    ],
                   ),
                 ),
-                alignment: Alignment.center,
               ),
-            ),
-          
-          ],
+              const SizedBox(height: 10,),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const QuizPage()));
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: mobileBackgroundColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow:[
+                      BoxShadow(
+                        color: mobileBackgroundColor.withOpacity(0.6),
+                        offset: const Offset(
+                          0.0,
+                          10.0,
+                        ),
+                        blurRadius: 10.0,
+                        spreadRadius: -6.0,
+                      ),  
+                    ],
+                    image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                        mobileBackgroundColor.withOpacity(0.35),
+                        BlendMode.multiply,
+                      ),
+                      image: const NetworkImage('https://immigrantinvest.com/wp-content/uploads/2016/12/50-european-dishes.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
+                    children: const [
+                      Align(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Text(
+                            "Кухонное меню",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              InkWell(
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const RulesScreen()));
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                  width: MediaQuery.of(context).size.width,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: mobileBackgroundColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow:[
+                      BoxShadow(
+                        color: mobileBackgroundColor.withOpacity(0.6),
+                        offset: const Offset(
+                          0.0,
+                          10.0,
+                        ),
+                        blurRadius: 10.0,
+                        spreadRadius: -6.0,
+                      ),  
+                    ],
+                    image: DecorationImage(
+                      colorFilter: ColorFilter.mode(
+                        mobileBackgroundColor.withOpacity(0.35),
+                        BlendMode.multiply,
+                      ),
+                      image: const NetworkImage('https://firebasestorage.googleapis.com/v0/b/dublinpub-a39c8.appspot.com/o/photo_2022-05-17_12-15-42.jpg?alt=media&token=3ac4154e-05b9-41a2-b401-182ff557d92f'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
+                    children: const [
+                      Align(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Text(
+                            "Все правилы - Dublin Pub",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10,),
+              // const Spacer(),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 22,),
+                width: MediaQuery.of(context).size.width,
+                height: 40,            
+                // color: const Color.fromARGB(255, 40, 99, 42),
+                decoration:  const BoxDecoration(
+                  color: Color(0xFF046936), //new Color.fromRGBO(255, 0, 0, 0.0),
+                  borderRadius:  BorderRadius.only(
+                    topLeft:  Radius.circular(40.0),
+                    topRight: Radius.circular(40.0))
+                ),
+                child: const Align(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Text(
+                      "Обслуживание 15%",
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                ),
+              ),
+            
+            ],
+          ),
         ),
       ),
     );
